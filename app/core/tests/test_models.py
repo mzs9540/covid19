c2bafbdd068ef51cf41dbfdc00acad5bc900aec9
MZS <zaidsiddiqui50@gmail.com>
2020-04-10 21:06:15 +0530

    added postgres support

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,12 +2,17 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 
+def sample_user(email='test@mzs.com', password='pssword123'):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTests(TestCase):
 
-    def test_create_user_with_email_successful(self):
-        """Test creating a new user with an email is successful"""
-        email = 'test@londonappdev.com'
-        password = 'Password123'
+    def test_create_user_with_email_succesful(self):
+        """Test Creating new user with an email is successful"""
+        email = 'test@mzs.com'
+        password = 'Testpass123'
         user = get_user_model().objects.create_user(
             email=email,
             password=password
@@ -17,13 +22,23 @@ class ModelTests(TestCase):
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
-        """Test the email for a new user is normalized"""
-        email = 'test@LONDONAPPDEV.com'
+        """Test the new user is Normalized"""
+        email = 'test@Mzs.com'
         user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
 
     def test_new_user_invalid_email(self):
-        """Test creating user with no email raises error"""
+        """Test creating user no email raises error"""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user(None, 'test123')
+            get_user_model().objects.create_user(None, 'Test123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@mzs.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
--- app/covid19/settings.py
+++ app/covid19/settings.py
@@ -64,11 +64,14 @@ WSGI_APPLICATION = 'covid19.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
