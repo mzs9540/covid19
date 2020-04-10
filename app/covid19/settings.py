30805d23185f4086f61abc6f7a8dd5eceed62e0a
MZS <zaidsiddiqui50@gmail.com>
2020-04-10 20:18:00 +0530

    Start Project

--- app/covid19/settings.py
+++ app/covid19/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -106,3 +107,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL = 'core.User'
