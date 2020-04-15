d611aac18e3b893bea8bc4231874ed131616463f
MZS <zaidsiddiqui50@gmail.com>
2020-04-15 14:05:12 +0530

    adding server.sh and dockerfile edt

--- start-server.sh
+++ start-server.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 # start-server.sh
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     (python manage.py createsuperuser --no-input)
 fi
 
-(gunicorn covid19.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) &
+(gunicorn covid19.wsgi --user www-data --bind 0.0.0.0:8000 --workers 3) &
 nginx -g "daemon off;"
